9a75a7dcc118d233105d6c46eefaf0bbba253f58
Paul Wilkinson <paulw@wilko.me>
2019-01-17 12:50:12 +1100

    Initial Commit

---
+++ L2CapDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  L2CapDemo
+//
+//  Created by Paul Wilkinson on 17/1/19.
+//  Copyright © 2019 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ L2CapDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  L2CapDemo
+//
+//  Created by Paul Wilkinson on 17/1/19.
+//  Copyright © 2019 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
