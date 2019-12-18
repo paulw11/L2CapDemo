633ee5edb7dbc208306a7ad47fa9ad7f567842d2
Paul Wilkinson <paulw@wilko.me>
2019-12-18 13:58:02 +1100

    Updated to use L2Cap module

--- L2CapDemo/CentralViewController.swift
+++ L2CapDemo/CentralViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 import CoreBluetooth
 import ExternalAccessory
+import L2Cap
 
 class CentralViewController: UIViewController {
     
     @IBOutlet weak var scanSwitch: UISwitch!
     @IBOutlet weak var inputText: UITextField!
     @IBOutlet weak var byteLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
     
     private var peripheral: CBPeripheral?
-    private var channel: CBL2CAPChannel?
+    private var connection: L2CapConnection?
     private var characteristic: CBCharacteristic?
+    
+    private var l2capCentral: L2CapCentral!
     
     private var bytesSent = 0 {
         didSet {
@@ -30,176 +34,42 @@ class CentralViewController: UIViewController {
     
     private var outputData = Data()
     
-    var central:CBCentralManager!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.central = CBCentralManager(delegate: self, queue: nil)
         self.bytesSent = 0
+        self.l2capCentral = L2CapCentral()
+        self.l2capCentral.discoveredPeripheralCallback = { peripheral in
+            self.peripheral = peripheral
+            self.l2capCentral.connect(peripheral: peripheral) { connection in
+                self.connection = connection
+                self.connection?.receiveCallback = { (connection,data) in
+                    print("Received data")
+                }
+                self.connection?.sentDataCallback = { (connection, count) in
+                    self.bytesSent += count
+                }
+                DispatchQueue.main.async {
+                    self.sendButton.isEnabled = true
+                }
+            }
+        }
     }
     
     @IBAction func scanSwitched(_ sender: UISwitch) {
-        sender.isOn ? startScan():stopScan()
+         self.l2capCentral.scan = sender.isOn
     }
     
     @IBAction func sendTextTapped(_ sender: UIButton) {
+        guard let connection = self.connection else {
+            return
+        }
         var lngStr = "1234567890"
         for _ in 1...10 {lngStr = lngStr + lngStr}
-        for _ in 1...10 {
+       for _ in 1...10 {
         let data = lngStr.data(using: .utf8)!
-        self.queue(data:data)
-        }
-    }
-    
-    private func queue(data: Data) {
-        queueQueue.sync  {
-            self.outputData.append(data)
-        }
-        self.send()
-    }
-    
-    private func send() {
-        
-        guard let ostream = self.channel?.outputStream, !self.outputData.isEmpty, ostream.hasSpaceAvailable  else{
-            return
-        }
-        let bytesWritten =  outputData.withUnsafeBytes { ostream.write($0, maxLength: self.outputData.count) }
-        print("bytesWritten = \(bytesWritten)")
-        self.bytesSent+=bytesWritten
-        queueQueue.sync {
-            if bytesWritten < outputData.count {
-                outputData = outputData.advanced(by: bytesWritten)
-            } else {
-                outputData.removeAll()
-            }
-        }
-    }
-    
-    func startScan() {
-        self.central.scanForPeripherals(withServices: [Constants.serviceID], options: nil)
-    }
-    
-    func stopScan() {
-        self.scanSwitch.isOn = false
-        self.central.stopScan()
-    }
-}
-
-extension CentralViewController: CBCentralManagerDelegate {
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        let poweredOn = central.state == .poweredOn
-        self.scanSwitch.isEnabled = poweredOn
-        self.inputText.isEnabled = poweredOn
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        self.peripheral = peripheral
-        peripheral.delegate = self
-        print("Discovered \(peripheral)")
-        self.central.connect(peripheral, options: nil)
-        self.stopScan()
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        self.central.connect(peripheral, options: nil)
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        peripheral.discoverServices([Constants.serviceID])
-    }
-    
-}
-
-extension CentralViewController: CBPeripheralDelegate {
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        if let error = error {
-            print("Service discovery error - \(error)")
-            return
-        }
-        
-        
-        
-        for service in peripheral.services ?? [] {
-            print("Discovered service \(service)")
-            if service.uuid == Constants.serviceID {
-                peripheral.discoverCharacteristics(nil, for: service)
-            }
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        if let error = error {
-            print("Characteristic discovery error - \(error)")
-            return
-        }
-        
-        for characteristic in service.characteristics ?? [] {
-            print("Discovered characteristic \(characteristic)")
-            if characteristic.uuid ==  Constants.PSMID {
-                self.characteristic = characteristic
-                peripheral.setNotifyValue(true, for: characteristic)
-                peripheral.readValue(for: characteristic)
-            }
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        if let error = error {
-            print("Characteristic update error - \(error)")
-            return
-        }
-        
-        print("Read characteristic \(characteristic)")
-        
-        if let dataValue = characteristic.value, let string = String(data: dataValue, encoding: .utf8), let psm = UInt16(string) {
-                print("Opening channel \(psm)")
-                self.peripheral?.openL2CAPChannel(psm)
-        } else {
-            print("Problem decoding PSM")
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didOpen channel: CBL2CAPChannel?, error: Error?) {
-        if let error = error {
-            print("Error opening l2cap channel - \(error.localizedDescription)")
-            return
-        }
-        guard let channel = channel else {
-            return
-        }
-        print("Opened channel \(channel)")
-        self.channel = channel
-        channel.inputStream.delegate = self
-        channel.outputStream.delegate = self
-        print("Opened channel \(channel)")
-        channel.inputStream.schedule(in: RunLoop.current, forMode: .default)
-        channel.outputStream.schedule(in: RunLoop.current, forMode: .default)
-        channel.inputStream.open()
-        channel.outputStream.open()
-    }
-    
-
-}
-
-extension CentralViewController: StreamDelegate {
-    
-    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
-        switch eventCode {
-        case Stream.Event.openCompleted:
-            print("Stream is open")
-        case Stream.Event.endEncountered:
-            print("End Encountered")
-        case Stream.Event.hasBytesAvailable:
-            print("Bytes are available")
-        case Stream.Event.hasSpaceAvailable:
-            print("Space is available")
-            self.send()
-        case Stream.Event.errorOccurred:
-            print("Stream error")
-        default:
-            print("Unknown stream event")
+            connection.send(data: data)
         }
     }
 }
+    
--- L2CapDemo/PeripheralViewController.swift
+++ L2CapDemo/PeripheralViewController.swift
@@ -8,19 +8,15 @@
 
 import UIKit
 import CoreBluetooth
+import L2Cap
 
 class PeripheralViewController: UIViewController {
     
     @IBOutlet weak var advertiseSwitch: UISwitch!
     @IBOutlet weak var outputLabel: UILabel!
     
-    private var service: CBMutableService!
-    private var characteristic: CBMutableCharacteristic!
-    private var channel: CBL2CAPChannel?
-    private var channelPSM: UInt16?
-    
-    var peripheralManager: CBPeripheralManager!
-    var subscribedCentrals = [CBCharacteristic:[CBCentral]]()
+    private var peripheral: L2CapPeripheral!
+    private var connection: L2CapConnection?
     
     private var bytesReceived = 0 {
         didSet {
@@ -33,118 +29,18 @@ class PeripheralViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        self.peripheral = L2CapPeripheral(connectionHandler: { (connection) in
+            self.connection = connection
+            self.connection?.receiveCallback = { (connection, data) in
+                DispatchQueue.main.async {
+                    self.bytesReceived += data.count
+                }
+            }
+        })
         self.bytesReceived = 0
     }
     
     @IBAction func advertiseSwitched(_ sender: UISwitch) {
-        sender.isOn ? startAdvertise():stopAdvertise()
-    }
-    
-    func startAdvertise() {
-        self.service = CBMutableService(type: Constants.serviceID, primary: true)
-        self.characteristic = CBMutableCharacteristic(type: Constants.PSMID, properties: [ CBCharacteristicProperties.read, CBCharacteristicProperties.indicate], value: nil, permissions: [CBAttributePermissions.readable] )
-        self.service.characteristics = [self.characteristic]
-        self.peripheralManager.add(self.service)
-        self.peripheralManager.publishL2CAPChannel(withEncryption: false)
-        self.peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey : [Constants.serviceID]])
-    }
-    
-    func stopAdvertise() {
-        self.advertiseSwitch.isOn = false
-        self.peripheralManager.stopAdvertising()
-    }
-    
-}
-
-
-extension PeripheralViewController: CBPeripheralManagerDelegate {
-    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        let poweredOn = peripheral.state == .poweredOn
-        self.advertiseSwitch.isEnabled = poweredOn
-    }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        var centrals = self.subscribedCentrals[characteristic, default: [CBCentral]()]
-        centrals.append(central)
-        self.subscribedCentrals[characteristic]  = centrals
-    }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, didPublishL2CAPChannel PSM: CBL2CAPPSM, error: Error?) {
-        if let error = error {
-            print("Error publishing channel: \(error.localizedDescription)")
-            return
-        }
-        print("Published channel \(PSM)")
-        
-        self.channelPSM = PSM
-        
-        if let data = "\(PSM)".data(using: .utf8) {
-            
-            self.characteristic.value = data
-            
-            self.peripheralManager.updateValue(data, for: self.characteristic, onSubscribedCentrals: self.subscribedCentrals[self.characteristic])
-        }
-        
-    }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        if let psm = self.channelPSM, let data = "\(psm)".data(using: .utf8) {
-            request.value = characteristic.value
-            print("Respond \(data)")
-            self.peripheralManager.respond(to: request, withResult: .success)
-        } else {
-            self.peripheralManager.respond(to: request, withResult: .unlikelyError)
-        }
-    }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, didOpen channel: CBL2CAPChannel?, error: Error?) {
-        if let error = error {
-            print("Error opening channel: \(error.localizedDescription)")
-            return
-        }
-        self.channel = channel
-        if let channel = self.channel {
-            print("Opened channel \(channel)")
-            channel.inputStream.delegate = self
-            channel.outputStream.delegate = self
-            channel.inputStream.schedule(in: RunLoop.current, forMode: .common)
-            channel.outputStream.schedule(in: RunLoop.current, forMode: .common)
-            channel.inputStream.open()
-            channel.outputStream.open()
-        }
-    }
-    
-    func readBytes(from stream: InputStream) {
-        let bufLength = 1024
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufLength)
-        let bytesRead = stream.read(buffer, maxLength: bufLength)
-        print("bytesRead = \(bytesRead)")
-        self.bytesReceived += bytesRead
-        if stream.hasBytesAvailable {
-            self.readBytes(from: stream)
-        }
-    }
-    
-}
-
-extension PeripheralViewController: StreamDelegate {
-    
-    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
-        switch eventCode {
-        case Stream.Event.openCompleted:
-            print("Stream is open")
-        case Stream.Event.endEncountered:
-            print("End Encountered")
-        case Stream.Event.hasBytesAvailable:
-            print("Bytes are available")
-            self.readBytes(from: aStream as! InputStream)
-        case Stream.Event.hasSpaceAvailable:
-            print("Space is available")
-        case Stream.Event.errorOccurred:
-            print("Stream error")
-        default:
-            print("Unknown stream event")
-        }
+      self.peripheral.publish = sender.isOn
     }
 }
